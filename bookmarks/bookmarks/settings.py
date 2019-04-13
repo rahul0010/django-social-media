6f7e20c4f3beb5c8ad09936050bf3bcfafe00990
Rahul Bharati <rahul@rahul-pc.localdomain>
2019-04-13 09:45:09 +0530

    user profile completed

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -134,3 +134,7 @@ EMAIL_HOST_PASSWORD = 'projectsindjango'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+#Media Files or uploaded files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
