ba7bfc8a6a793cefe5918bf145feb36db23cd7f4
Rahul Bharati <rahul@rahul-pc.localdomain>
2019-04-28 11:24:15 +0530

    sharing content on a website

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'social_django',
     'images.apps.ImagesConfig',
+    'sorl.thumbnail',
 ]
 
 MIDDLEWARE = [
--- bookmarks/images/static/js/bookmarklet.js
+++ bookmarks/images/static/js/bookmarklet.js
@@ -1,6 +1,6 @@
 (function(){
     var jquery_version = '3.3.1';
-    var site_url = 'http://5b9a373b.ngrok.io/';
+    var site_url = 'https://5b9a373b.ngrok.io/';
     var static_url = site_url + 'static/';
     var min_width = 100;
     var min_height = 100;
