fcd9264ffec1cb1e3f53c8ec52fc7e80514e9297
Rahul Bharati <rahul@rahul-pc.localdomain>
2019-04-28 11:42:52 +0530

    user system

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
@@ -69,3 +70,12 @@ def edit(request):
         profile_form = ProfileEditForm(instance=request.user.profile)
     return render(request,'account/edit.html',{'user_form': user_form,'profile_form': profile_form})
 
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(request,'account/user/list.html',{'section': 'people','users': users})
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User,username=username,is_active=True)
+    return render(request,'account/user/detail.html',{'section': 'people','user': user})
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from django.urls import reverse_lazy
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -108,6 +110,10 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda u: reverse_lazy('user_detail',args=[u.username])
+}
 
 
 # Internationalization
